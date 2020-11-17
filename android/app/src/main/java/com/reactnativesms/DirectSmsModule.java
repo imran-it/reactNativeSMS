185e7a538220693dae7e41d19a98655f02069899
Imran Khan <emailofimran1992@gmail.com>
2020-11-17 12:00:31 +0600

    Version 1.0   send sms without message

--- android/app/src/main/java/com/reactnativesms/DirectSmsModule.java
+++ android/app/src/main/java/com/reactnativesms/DirectSmsModule.java
@@ -1,6 +1,5 @@
 package com.reactnativesms;
 
-
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.Callback;
@@ -8,26 +7,28 @@ import com.facebook.react.bridge.ReactMethod;
 import com.facebook.react.uimanager.IllegalViewOperationException;
 import android.telephony.SmsManager;
 
- 
 public class DirectSmsModule extends ReactContextBaseJavaModule {
- 
+
     public DirectSmsModule(ReactApplicationContext reactContext) {
-        super(reactContext); //required by React Native
+        super(reactContext); // required by React Native
     }
- 
+
     @Override
-    //getName is required to define the name of the module represented in JavaScript
+    // getName is required to define the name of the module represented in
+    // JavaScript
     public String getName() {
         return "DirectSms";
     }
- 
+
     @ReactMethod
     public void sendDirectSms(String numbers, String msg) {
-        try {    
+        try {
             SmsManager smsManager = SmsManager.getDefault();
-            smsManager.sendTextMessage(numbers, null, msg, null, null);   
+            smsManager.sendTextMessage(numbers, null, msg, null, null);
+
         } catch (Exception ex) {
             System.out.println("couldn't send message.");
         }
     }
+
 }
--- src/component/SMSHandler/SMSHandler.js
+++ src/component/SMSHandler/SMSHandler.js
@@ -1,163 +1,8 @@
-// // import React from 'react';
-
-// // import {View, Text, ImageBackground, StatusBar} from 'react-native';
-
-
-// // Example to Send Text SMS on Button Click in React Native
-// // https://aboutreact.com/send-text-sms-in-react-native/
-
-// // import React in our code
-// import React, { useState } from 'react';
-
-// // import all the components we are going to use
-// import {
-//   SafeAreaView,
-//   StyleSheet,
-//   View,
-//   Text,
-//   TouchableOpacity,
-//   TextInput,
-// } from 'react-native';
-
-// // import SMS API
-// import SendSMS from 'react-native-sms';
-
-// const App = () => {
-//   const [mobileNumber, setMobileNumber] = useState('9999999999');
-//   const [bodySMS, setBodySMS] = useState(
-//     'Please follow https://aboutreact.com',
-//   );
-
-//   const initiateSMS = () => {
-//     // Check for perfect 10 digit length
-//     if (mobileNumber.length != 10) {
-//       alert('Please insert correct contact number');
-//       return;
-//     }
-
-//     SendSMS.send(
-//       {
-//         // Message body
-//         body: bodySMS,
-//         // Recipients Number
-//         recipients: [mobileNumber],
-//         // An array of types 
-//         // "completed" response when using android
-//         successTypes: ['sent', 'queued'],
-//       },
-//       (completed, cancelled, error) => {
-//         if (completed) {
-//           console.log('SMS Sent Completed');
-//         } else if (cancelled) {
-//           console.log('SMS Sent Cancelled');
-//         } else if (error) {
-//           console.log('Some error occured');
-//         }
-//       },
-//     );
-//   };
-
-//   return (
-//     <SafeAreaView style={styles.container}>
-//       <View style={styles.container}>
-//         <Text style={styles.titleText}>
-//           Example to Send Text SMS on Button Click in React Native
-//         </Text>
-//         <Text style={styles.titleTextsmall}>
-//           Enter Mobile Number
-//         </Text>
-//         <TextInput
-//           value={mobileNumber}
-//           onChangeText={
-//             (mobileNumber) => setMobileNumber(mobileNumber)
-//           }
-//           placeholder={'Enter Conatct Number to Call'}
-//           keyboardType="numeric"
-//           style={styles.textInput}
-//         />
-//         <Text style={styles.titleTextsmall}>
-//           Enter SMS body
-//         </Text>
-//         <TextInput
-//           value={bodySMS}
-//           onChangeText={(bodySMS) => setBodySMS(bodySMS)}
-//           placeholder={'Enter SMS body'}
-//           style={styles.textInput}
-//         />
-//         <TouchableOpacity
-//           activeOpacity={0.7}
-//           style={styles.buttonStyle}
-//           onPress={initiateSMS}>
-//           <Text style={styles.buttonTextStyle}>
-//             Send Message
-//           </Text>
-//         </TouchableOpacity>
-//       </View>
-//     </SafeAreaView>
-//   );
-// };
-
-// export default App;
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: 'white',
-//     padding: 10,
-//     textAlign: 'center',
-//   },
-//   titleText: {
-//     fontSize: 22,
-//     textAlign: 'center',
-//     fontWeight: 'bold',
-//   },
-//   titleTextsmall: {
-//     marginVertical: 8,
-//     fontSize: 16,
-//   },
-//   buttonStyle: {
-//     justifyContent: 'center',
-//     marginTop: 15,
-//     padding: 10,
-//     backgroundColor: '#8ad24e',
-//   },
-//   buttonTextStyle: {
-//     color: '#fff',
-//     textAlign: 'center',
-//   },
-//   textInput: {
-//     height: 40,
-//     borderColor: 'gray',
-//     borderWidth: 1,
-//     width: '100%',
-//     paddingHorizontal: 10,
-//   },
-// });
-
-// // const SMSHandler = () => {
-// //   // const themeColor = useSelector((state) => state.Settings.ThemeColor);
-
-// //   // const dispatch = useDispatch();
-
-// //   // const Styles = styles();
-
-// //   return (
-// //     <>
-// //       <View>
-// //         <Text>ok</Text>
-// //       </View>
-// //     </>
-// //   );
-// // // };
-
-// // export default SendSMS;
-
-
-
-
 import React from 'react';
 import { NativeModules, View, TouchableOpacity, Text } from 'react-native';
 import { PermissionsAndroid } from 'react-native';
+
+
 var DirectSms = NativeModules.DirectSms;
 
 export default class App extends React.Component {
@@ -165,7 +10,7 @@ export default class App extends React.Component {
   async sendDirectSms() {
     try {
 
-      console.log("okokokok")
+
       const granted = await PermissionsAndroid.request(
         PermissionsAndroid.PERMISSIONS.SEND_SMS,
         {
@@ -178,8 +23,13 @@ export default class App extends React.Component {
           buttonPositive: 'OK',
         },
       );
+
+
+
       if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-        DirectSms.sendDirectSms('0935', 'This is a direct message');
+        //  DirectSms.sendDirectSms('045549044488', 'This is a direct message');
+
+        console.log('DirectSms', DirectSms)
       } else {
         console.log('SMS permission denied');
       }
